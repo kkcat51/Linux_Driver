64d75e777f66c01f07d76fb7e1a1221f0b96a83a
kkcat51 <383839105@qq.com>
2018-05-15 22:42:20 -0700

    字符驱动方式1:原始方式

---
+++ 1.charDriver_origin/leds_drivers.c
@@ -0,0 +1,53 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/gpio.h>
+#include <plat/gpio-cfg.h>
+#include <mach/gpio.h>
+#include <mach/gpio-exynos4.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("kk");
+
+#define DEVICE "leds"
+
+int major;
+
+static int leds_open(struct inode* inode,struct file* file){
+	printk("led open\n");
+	return 0;
+}
+static int leds_release(struct inode* inode,struct file* file){
+	printk("leds release\n");
+	return 0;
+}
+static long leds_ioctl(struct file* file,unsigned int cmd,unsigned long arg){
+	printk("cmd is %d,arg is %d\n",cmd,arg);
+	if(cmd>1 || arg>1){
+		printk("input error\n");
+	}
+	gpio_set_value(EXYNOS4_GPL2(0),cmd);
+	return 0;
+}
+
+static struct file_operations leds_ops = {
+	.owner = THIS_MODULE,
+	.open = leds_open,
+	.release = leds_release,
+	.unlocked_ioctl = leds_ioctl,
+};
+static int leds_init(){
+        major = register_chrdev(0,DEVICE,&leds_ops);
+	printk(KERN_INFO"major nums is:%d\n",major);
+	int ret = gpio_request(EXYNOS4_GPL2(0),"LED");
+	if(ret<0)
+		printk("gpio request failed!\n");
+	s3c_gpio_cfgpin(EXYNOS4_GPL2(0),S3C_GPIO_OUTPUT);
+	gpio_set_value(EXYNOS4_GPL2(0),0);
+}
+static int leds_exit(){
+	unregister_chrdev(major,DEVICE);
+	printk("unregister chrdev");
+}
+module_init(leds_init);
+module_exit(leds_exit);
