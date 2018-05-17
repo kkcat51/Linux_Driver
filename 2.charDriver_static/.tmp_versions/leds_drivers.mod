6624856977afd8ca1696f820ce2f4077d7bd6dcd
kkcat51 <383839105@qq.com>
2018-05-16 22:06:44 -0700

    字符驱动模板2:静态设定方式

---
+++ 2.charDriver_static/.tmp_versions/leds_drivers.mod
@@ -0,0 +1,2 @@
+/home/topeet/linux_driver/Linux_Driver/2.charDriver_static/leds_drivers.ko
+/home/topeet/linux_driver/Linux_Driver/2.charDriver_static/leds_drivers.o
---
+++ 2.charDriver_static/leds_app.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+int main(){
+	int fd = open("/dev/leds",O_RDWR|O_NDELAY);
+	if(fd<0)
+		printf("leds open fail\n");
+	else{
+		printf("leds open success\n");
+		ioctl(fd,1,1);
+		sleep(2);
+		ioctl(fd,0,1);
+		sleep(2);
+		ioctl(fd,1,1);
+	}
+	close(fd);
+}
