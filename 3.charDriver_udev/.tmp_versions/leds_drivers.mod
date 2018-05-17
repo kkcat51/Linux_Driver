38a9130153e3569030e622d1f2d6e6a48ee90c6d
kkcat51 <383839105@qq.com>
2018-05-16 22:48:21 -0700

    字符驱动方式3:udev自动生成设备文件

---
+++ 3.charDriver_udev/.tmp_versions/leds_drivers.mod
@@ -0,0 +1,2 @@
+/home/topeet/linux_driver/Linux_Driver/3.charDriver_udev/leds_drivers.ko
+/home/topeet/linux_driver/Linux_Driver/3.charDriver_udev/leds_drivers.o
---
+++ 3.charDriver_udev/leds_drivers.c
@@ -0,0 +1,81 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/gpio.h>
+#include <plat/gpio-cfg.h>
+#include <mach/gpio.h>
+#include <mach/gpio-exynos4.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("kk");
+
+#define MAJOR 210
+
+struct cdev *my_cdev;
+struct class *my_class;
+dev_t devId;
+
+static int leds_open(struct inode* inode,struct file* file){
+	printk("led open\n");
+	return 0;
+}
+static int leds_release(struct inode* inode,struct file* file){
+	printk("leds release\n");
+	return 0;
+}
+static long leds_ioctl(struct file* file,unsigned int cmd,unsigned long arg){
+	printk("cmd is %d,arg is %d\n",cmd,arg);
+	if(cmd>1 || arg>1){
+		printk("input error\n");
+		return -1;
+	}
+	gpio_set_value(EXYNOS4_GPL2(0),cmd);
+	return 0;
+}
+
+static struct file_operations leds_ops = {
+	.owner = THIS_MODULE,
+	.open = leds_open,
+	.release = leds_release,
+	.unlocked_ioctl = leds_ioctl,
+};
+static int leds_init(){
+
+	devId = MKDEV(MAJOR,0);//参数1为主设备号,参数2为次设备号
+	my_cdev = cdev_alloc();
+	cdev_init(my_cdev,&leds_ops);
+	int err = cdev_add(my_cdev,devId,1);//从设备号devId开始连续添加1个设备
+	if(err<0){
+		printk("cdev_add failed\n");
+		return -1;
+	}
+
+	my_class = class_create(THIS_MODULE,"led_class");
+	if(IS_ERR(my_class)){
+		printk("create class failed\n");
+		return -1;
+	}
+	device_create(my_class,NULL,devId,NULL,"leds""%d",MINOR(devId));
+
+	int ret = gpio_request(EXYNOS4_GPL2(0),"LED");
+	if(ret<0){
+		printk("gpio request failed!\n");
+		return -1;
+	}
+	s3c_gpio_cfgpin(EXYNOS4_GPL2(0),S3C_GPIO_OUTPUT);
+	gpio_set_value(EXYNOS4_GPL2(0),0);
+	return 0;
+}
+static void leds_exit(){
+	cdev_del(my_cdev);
+
+	device_destroy(my_class,devId);
+	class_destroy(my_class);
+
+	gpio_free(EXYNOS4_GPL2(0));
+	printk("cdev_del \n");
+}
+module_init(leds_init);
+module_exit(leds_exit);
